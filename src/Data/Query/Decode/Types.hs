b70ef17f0f1f2a4c8b419e04864dc6544c69bcbe
Ole Krüger <ole@vprsm.de>
2021-01-11 22:17:52 +0000

    Make use of Pretty for Decoder types

--- src/Data/Query/Decode/Types.hs
+++ src/Data/Query/Decode/Types.hs
@@ -10,30 +10,43 @@
 module Data.Query.Decode.Types where
 
 import           Control.Applicative.Free (Ap, runAp_)
+import           Data.Fix (Fix (Fix), foldFix)
 import           Data.Functor.Coyoneda (Coyoneda (..))
 import qualified Data.HashMap.Strict as HashMap
 import           Data.HashSet (HashSet)
-import qualified Data.List as List
+import qualified Data.Query.Shape as Shape
 import qualified Data.Query.Utilities as Utilities
 import qualified Data.Query.Value as Value
 import           Data.Scientific (Scientific)
 import           Data.Text (Text)
-import qualified Data.Text as Text
 import           Data.Vector (Vector)
+import qualified Prettyprinter as Pretty
 import qualified Type.Reflection as Reflection
 
+-- | Query for @a@
 data Query a = Query
   { query_type :: Reflection.TypeRep a
   , query_schema :: Maybe (Decoder a)
   }
+  deriving Show
 
-instance Show (Query a) where
-  show = show . query_type
+instance Pretty.Pretty (Query a) where
+  pretty = Shape.prettyQueryShape . queryToShape
 
+queryToShape :: Query a -> Shape.QueryShape
+queryToShape query = Fix Shape.QueryShape
+  { Shape.queryShape_type = Reflection.SomeTypeRep $ query_type query
+  , Shape.queryShape_shape = decoderToShape <$> query_schema query
+  }
+
+-- | Query for a variant constructor
 newtype ConstructorQuery a = ConstructorQuery
   { unConstructorQuery :: Coyoneda Query a }
   deriving newtype Functor
-  deriving Show via Utilities.CoyonedaShow Query a
+  deriving (Show, Pretty.Pretty) via Utilities.CoyonedaShow Query a
+
+constructorQueryToQueryShape :: ConstructorQuery a -> Shape.QueryShape
+constructorQueryToQueryShape (ConstructorQuery (Coyoneda _ query)) = queryToShape query
 
 data FieldQuery a where
   MandatoryFieldQuery
@@ -46,17 +59,36 @@ data FieldQuery a where
     -> Query a
     -> FieldQuery (Maybe a)
 
-instance Show (FieldQuery a) where
-  show = \case
-    MandatoryFieldQuery name query -> Text.unpack name <> " : " <> show query
-    OptionalFieldQuery name query -> Text.unpack name <> "? : " <> show query
+deriving instance Show (FieldQuery a)
+
+fieldQueryToFieldShape :: FieldQuery a -> HashMap.HashMap Text (Shape.FieldShapeF Shape.QueryShape)
+fieldQueryToFieldShape = \case
+  MandatoryFieldQuery name query -> HashMap.singleton name Shape.FieldShapeF
+    { Shape.fieldShape_schema = queryToShape query
+    , Shape.fieldShape_optional = False
+    }
+
+  OptionalFieldQuery name query -> HashMap.singleton name Shape.FieldShapeF
+    { Shape.fieldShape_schema = queryToShape query
+    , Shape.fieldShape_optional = True
+    }
 
 newtype FieldsDecoder a = FieldsDecoder
   { unFieldDecoder :: Ap FieldQuery a }
   deriving newtype (Functor, Applicative)
 
 instance Show (FieldsDecoder a) where
-  show (FieldsDecoder fields) = "{" <> List.intercalate ", " (runAp_ (pure . show) fields) <> "}"
+  show = show . Pretty.pretty
+
+instance Pretty.Pretty (FieldsDecoder a) where
+  pretty =
+    Utilities.runPrettyM
+    . Shape.prettyShapeF
+    . fmap (foldFix Shape.prettyQueryShapeF)
+    . Shape.Record . fieldDecoderToFieldShapes
+
+fieldDecoderToFieldShapes :: FieldsDecoder b -> HashMap.HashMap Text (Shape.FieldShapeF Shape.QueryShape)
+fieldDecoderToFieldShapes = runAp_ fieldQueryToFieldShape . unFieldDecoder
 
 data DecoderBase a where
   BoolDecoder
@@ -93,25 +125,41 @@ data DecoderBase a where
     -> DecoderBase a
 
 instance Show (DecoderBase a) where
-  show = \case
-    BoolDecoder -> "Bool"
-    NumberDecoder -> "Number"
-    StringDecoder -> "String"
-    NullableDecoder encoder -> "(" <> show encoder <> ")?"
-    ArrayDecoder encoder -> "[" <> show encoder <> "]"
-    StringMapDecoder encoder -> "{String => " <> show encoder <> "}"
-    EnumDecoder decoders -> "<" <> List.intercalate " | " (map Text.unpack (HashMap.keys decoders)) <> ">"
-    VariantDecoder decoders -> "<" <> List.intercalate " | " (map (\(key, encoder) -> Text.unpack key <> " : " <> show encoder) (HashMap.toList decoders)) <> ">"
-    RecordDecoder fields -> show fields
+  show = show . Pretty.pretty
+
+instance Pretty.Pretty (DecoderBase a) where
+  pretty =
+    Utilities.runPrettyM
+    . Shape.prettyShapeF
+    . fmap (foldFix Shape.prettyQueryShapeF)
+    . decoderBaseToShape
+
+decoderBaseToShape :: DecoderBase a -> Shape.ShapeF Shape.QueryShape
+decoderBaseToShape = \case
+  BoolDecoder -> Shape.Bool
+  NumberDecoder -> Shape.Number
+  StringDecoder -> Shape.String
+  NullableDecoder base -> Shape.Nullable $ decoderBaseToShape base
+  ArrayDecoder decoder -> Shape.Array $ queryToShape decoder
+  StringMapDecoder decoder -> Shape.StringMap $ queryToShape decoder
+  EnumDecoder items -> Shape.Enum $ HashMap.keys items
+  VariantDecoder constructors -> Shape.Variant $ constructorQueryToQueryShape <$> constructors
+  RecordDecoder fields -> Shape.Record $ fieldDecoderToFieldShapes fields
 
 newtype Decoder a = Decoder
   { unDecoder :: Coyoneda DecoderBase a }
   deriving newtype Functor
-  deriving Show via Utilities.CoyonedaShow DecoderBase a
+  deriving (Show, Pretty.Pretty) via Utilities.CoyonedaShow DecoderBase a
 
-data ConstructorMatch a = ConstructorMatch Text (ConstructorQuery a) Value.Value
+decoderToShape :: Decoder a -> Shape.ShapeF Shape.QueryShape
+decoderToShape (Decoder (Coyoneda _ base)) = decoderBaseToShape base
 
-deriving instance Show (ConstructorMatch a)
+data ConstructorMatch a = ConstructorMatch
+  { constructorMatch_name :: Text
+  , constructorMatch_query :: ConstructorQuery a
+  , constructorMatch_value :: Value.Value
+  }
+  deriving Show
 
 data DecodeError
   = forall a. UnexpectedInput (DecoderBase a) Value.NoCallValue
