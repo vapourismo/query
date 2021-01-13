2243122e858e2b8e8fc1ac3600937e5c85434132
Ole Krüger <ole@vprsm.de>
2021-01-13 18:44:20 +0000

    Remove redundant language extension

--- src/Data/Query/Schema.hs
+++ src/Data/Query/Schema.hs
@@ -4,7 +4,6 @@
 {-# LANGUAGE FlexibleContexts #-}
 {-# LANGUAGE FlexibleInstances #-}
 {-# LANGUAGE GADTs #-}
-{-# LANGUAGE LambdaCase #-}
 {-# LANGUAGE PolyKinds #-}
 {-# LANGUAGE RankNTypes #-}
 {-# LANGUAGE ScopedTypeVariables #-}
